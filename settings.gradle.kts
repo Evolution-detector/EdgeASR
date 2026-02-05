75bbf614d3115b2cf36ec3d241ce4d629739e015
Evolution-detector <64944480+Evolution-detector@users.noreply.github.com>
2026-02-05 09:16:06 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,21 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { setUrl("https://www.jitpack.io") }
-        mavenLocal()
+        // --- EdgeASR Added: JitPack Repository ---
+        // 这行代码允许我们直接引用 GitHub 上的开源项目
+        maven { url = uri("https://jitpack.io") }
     }
 }
-rootProject.name = "Voice-Recorder"
+
+rootProject.name = "Voice Recorder"
 include(":app")
